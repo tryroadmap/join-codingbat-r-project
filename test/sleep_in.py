3a5cd7dcf64f8756ce6657cc7367403a2754318c
Hoofar Pourzand <support@lotustech.io>
2020-03-24 17:37:58 -0400

    repo upload

---
+++ test/sleep_in.py
@@ -0,0 +1,11 @@
+def sleep_in():
+    """
+    input
+    output
+    logic
+    """
+with open('./some_log.txt', 'w') as f:
+    f.write('sleep_in test results')
+
+if __name__ == "__main__":
+    sleep_in = sleep_in()
